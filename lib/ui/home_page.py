4f3d81e7c8e17833950553ca1e1c35c9409fe37f
PallaviGJ <pallavigj125@gmail.com>
2020-06-12 16:28:42 +0530

    Added Succesfully

--- lib/ui/home_page.py
+++ lib/ui/home_page.py
@@ -13,6 +13,19 @@ class HomePage:
         except:
             return None
 
+    def get_user_tab(self):
+        try:
+            return self.driver.find_element_by_xpath("//div[text()='Users']")
+        except:
+            return None
+
+    def get_time_drop_down(self):
+        try:
+            return self.driver.find_element_by_xpath("//span[text()='Me']")
+        except:
+            return None
+
     def wait_for_home_page_to_load(self):
         wait = WebDriverWait(timeout=30,driver=self.driver)
         wait.until(expected_conditions.visibility_of(self.get_logout_button()))
+        wait.until(expected_conditions.visibility_of(self.get_user_tab()))
